d21c5166fa566aef4b9a7b52a9aa36a39ab7508c
Minhyeok-Oh <alsgur2848@daum.net>
2021-03-09 14:08:27 +0900

    todolist

--- cloneCoding/vanilaJS/Momentum/todo.js
+++ cloneCoding/vanilaJS/Momentum/todo.js
@@ -4,7 +4,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,8 +26,9 @@ function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = toDos.length + 1;
-    delBtn.innerHTML = "❌";
+    const newId = toDos.length + 1; 
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
